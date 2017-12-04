c19620c643320b3680d1aeee35412f2aaa27866a
Benny <carlsson.benny93@gmail.com>
2017-12-04 20:48:39 +0100

    added slider

--- src/Index.js
+++ src/Index.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Slider } from "react-native"
 
 export default class Index extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Lorem Ipsum</Text>
+        <Slider
+          maximumValue={250}
+          minimumValue={2}
+          minimumTrackTintColor={"#fff0"}
+          style={{ width: 200 }}
+          value={50}
+        />
       </View>
     )
   }
