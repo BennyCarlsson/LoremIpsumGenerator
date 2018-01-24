3a050607735bc6798c9d1c2901199c70117d42b7
Benny <carlsson.benny93@gmail.com>
2018-01-24 21:23:49 +0100

    added functionality for - and + 1

--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -1,25 +1,29 @@
 import React from "react"
-import { StyleSheet, Slider } from "react-native"
+import { StyleSheet, Slider, View, Text, TouchableOpacity } from "react-native"
 import { connect } from "react-redux"
 import { onSliderChangeValue, onSliderComplete } from "../actions"
 
 class MySlider extends React.Component {
   render() {
     return (
-      <Slider
-        maximumValue={this.props.maxValue}
-        minimumValue={this.props.minValue}
-        minimumTrackTintColor={"#fff0"}
-        thumbTintColor={"#03a4b3"}
-        style={styles.slider}
-        value={this.props.sliderValue}
-        onValueChange={n => {
-          this.props.onSliderChange(n)
-        }}
-        onSlidingComplete={n => {
-          this.props.onSliderComplete(n)
-        }}
-      />
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={() => this.props.onSliderChange(this.props.sliderValue - 1)}><Text>-</Text></TouchableOpacity>
+        <Slider
+          maximumValue={this.props.maxValue}
+          minimumValue={this.props.minValue}
+          minimumTrackTintColor={"#fff0"}
+          thumbTintColor={"#03a4b3"}
+          style={styles.slider}
+          value={this.props.sliderValue}
+          onValueChange={n => {
+            this.props.onSliderChange(n)
+          }}
+          onSlidingComplete={n => {
+            this.props.onSliderComplete(n)
+          }}
+        />
+        <TouchableOpacity onPress={() => this.props.onSliderChange(this.props.sliderValue + 1)}><Text>+</Text></TouchableOpacity>
+      </View>
     )
   }
 }
@@ -47,6 +51,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(MySlider)
 const styles = StyleSheet.create({
   slider: {
     height: 45,
-    width: "100%"
+    width: "90%"
   }
 })
